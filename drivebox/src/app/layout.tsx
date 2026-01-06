19295c782071d9e8fe601b0c4c87ac81402a9a59
architashinde <architashinde45@gmail.com>
2026-01-07 01:44:17 +0530

    initial commit for drivebox

---
+++ drivebox/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "DriveBox",
+  description: "DriveBox - Your Personal Cloud Storage Solution",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.variable} font-poppinsantialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
