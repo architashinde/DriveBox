9d9de3533b5a727d28a537cff3bc1f6ada43e96b
architashinde <architashinde45@gmail.com>
2026-02-19 01:11:56 +0530

    dashboard

--- drivebox/components/Sidebar.tsx
+++ drivebox/components/Sidebar.tsx
@@ -1,13 +1,25 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";  
+import React from "react";   
 
 const Sidebar = () => {
     return(
         <aside className="sidebar">
-            <Link href="/"><Image 
+            <Link href="/">
+            <Image 
             src="/logoo.png" 
-            alt="logo" width={160} height={50} className="hidden h-auto lg:block" /></Link>    
+            alt="logo" width={50} height={50} className="hidden h-auto lg:block" />
+            <Image src ="/logoo.png" alt="logo" width={32} height={32} className="lg:hidden" />
+            </Link> 
+
+            <nav className="mt-10">
+                <ul className="flex flex-1 flex-col gap-6">
+                    {[
+                        
+                    ]}
+                </ul>
+            </nav>
+
         </aside>
     )
 };
---
+++ drivebox/constants/index.ts
@@ -0,0 +1,5 @@
+export const navItems = [
+    {name: 'Dashboard', icon:"/public/dashboard.svg", href: '/dashboard'},
+    {name: 'Files', href: '/files'},
+    {name: 'Settings', href: '/settings'},
+];
