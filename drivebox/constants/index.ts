a5a5acc981491008142c34c67a110b7eb03d9bb8
architashinde <architashinde45@gmail.com>
2026-02-26 01:25:14 +0530

    .

--- drivebox/constants/index.ts
+++ drivebox/constants/index.ts
@@ -1,5 +1,6 @@
 export const navItems = [
     {name: 'Dashboard', icon:"/public/dashboard.svg", href: '/dashboard'},
     {name: 'Files', href: '/files'},
-    {name: 'Settings', href: '/settings'},
+    {name: 'Settings', href: '/settings'}
+
 ];
